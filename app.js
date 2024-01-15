c3fb02a02b9dfe075d22a858010c84237a36bf62
Dmytro <dimapen57@gmail.com>
2024-01-15 13:22:07 +0000

    GET /api

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const {getTopics} = require('./controllers/TopicsControllers')
+const {getTopics,getApi} = require('./controllers/TopicsControllers')
 const app = express();
 app.use(express.json());
 
 
 app.get('/api/topics',getTopics)
-
+app.get('/api',getApi)
 
 
 module.exports = app;
