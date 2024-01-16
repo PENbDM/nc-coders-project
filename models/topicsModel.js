b980d3e0ec04b52748a61e32a2d456c3507921d1
Dmytro <dimapen57@gmail.com>
2024-01-16 12:21:49 +0000

    made changes regarding nc-christian-dutton

--- models/topicsModel.js
+++ models/topicsModel.js
@@ -2,7 +2,6 @@ const db = require("../db/connection");
 
 const fetchTopics = () => {
     let querySort = `SELECT * FROM topics`;
-    console.log(db.query(querySort));
     return db.query(querySort)
 };
 
