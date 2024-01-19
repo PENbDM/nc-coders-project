cb9ab22b1ce09dd52d93e280282764c25c6b7bc2
Dmytro <dimapen57@gmail.com>
2024-01-19 09:58:59 +0000

    port listen

--- listen.js
+++ listen.js
@@ -1,3 +1,3 @@
 const app = require('./app');
-const port = 4000
-app.listen(4000, () => console.log(`server listening on port ${port}`));
+const {PORT=9090} = process.env;
+app.listen(PORT, () => console.log(`server listening on port ${PORT}`));
